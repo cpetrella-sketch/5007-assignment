c409bee7af755db22ffce2cb312c9a256fc20953
cpetrella-sketch <78937129+cpetrella-sketch@users.noreply.github.com>
2022-11-13 21:38:03 -0500

    Skeleton code

---
+++ main.py
@@ -0,0 +1,26 @@
+
+def main():
+    print("test")
+def add():
+    #Create a new instance of Task class and adds it to calendar-list
+    sort()
+    #Sorts the list of tasks
+    return
+def delete():
+    #Deletes given instance of Task class and clears from list
+    sort()
+    #Re-sorts list of tasks
+    return
+def update():
+    #Edits a task without needing to delete and add a class
+    sort()
+    #Sorts the list of tasks now that it's changed
+    return
+def sort():
+    #Organizes the list of tasks based on a set of criteria, probably by date
+    return
+def printCalendar():
+    #Outputs the current task list as a csv file
+    return
+if __name__ == "__main__":
+    main()
---
+++ task.py
@@ -0,0 +1,4 @@
+class Task(date):
+    def __init__(self):
+        self.date = "00/00/0000"
+        self.name = "Placeholder"
