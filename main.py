5f1ae7c8dbb7907e1e45ba72503aa266da0c6c91
cpetrella-sketch <78937129+cpetrella-sketch@users.noreply.github.com>
2022-12-15 11:02:50 -0500

    Share function.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # CS5007
+import csv
+
 from Task import Task
 from Application import Application
 
@@ -6,8 +8,19 @@ from tkinter import *
 from tkinter import ttk
 import tkinter as tk
 
+<<<<<<< Updated upstream
 all_task = ["No_Name", "No_Date", "To Do"]
 
+=======
+#all_task = []
+all_task = ["No_Name", "No_Date", "Doing"]
+all_Doing_task = {}
+share_todo = []
+share_doing = []
+share_done = []
+#all_Doing_task = []
+#all_Done_task = []
+>>>>>>> Stashed changes
 def enter_release_text_handler_entry(event):
     print("Text entered = " + event.widget.get())
 
@@ -32,17 +45,54 @@ def button_lambda_handler_updates(widget):
 def combobox_handler(event):
     print(event.widget["text"] + " Selected = " + str(event.widget.get()))
 
+<<<<<<< Updated upstream
 def createTask(widget, todo, doing, done, box):
     global all_task
+=======
+def sort_doing(all_Doing_task, todo, doing, done, widget):
+    listDate = []
+    # Step 1: delete all tasks in Doing
+    for key in all_Doing_task:
+        listDate.append(key)
+        frame = all_Doing_task[key][1]
+        delete_frame(frame)
+
+    # Step 2: sort all tasks based on due date
+    listDate.sort(key=lambda date: datetime.strptime(date, "%d-%m-%Y "))
+    print(listDate)
+    new_list_task = []
+    for d in listDate:
+        new_list_task = [all_Doing_task[d][0], d, "Doing"]
+        print("print in all task")
+        print()
+        root = all_Doing_task[d][1]
+        #all_task = new_list_task
+        #print(all_task)
+        createTask(widget, todo, doing, done, root, new_list_task)
+    # step 3: loop through list of sorted task to create new tasks in Doing
+
+
+def createTask(widget, todo, doing, done, box, all_task):
+    temp = all_task[2]
+>>>>>>> Stashed changes
     print("confirmed " + widget["text"] + " TASK CREATED")
     newTask = Task(all_task[0], all_task[1], all_task[2])
     print(newTask.getStatus())
     if all_task[2] == 'To Do':
         clmn = todo
+        share_todo.append([all_task[0], all_task[1]])
     if all_task[2] == 'Doing':
         clmn = doing
+<<<<<<< Updated upstream
+=======
+        share_doing.append([all_task[0], all_task[1]])
+        all_Doing_task[all_task[1]] = [all_task[0]]   # assign date key to name
+        print("print current all_DOING")
+        print(all_Doing_task)
+>>>>>>> Stashed changes
     if all_task[2] == 'Done':
         clmn = done
+        share_done.append([all_task[0], all_task[1]])
     task_frame = Frame(clmn, highlightcolor="blue", highlightbackground="blue", highlightthickness=5)
     task_frame.pack()
     name_label = ttk.Label(task_frame)
@@ -51,10 +101,10 @@ def createTask(widget, todo, doing, done, box):
     date_label = ttk.Label(task_frame)
     date_label["text"] = all_task[1]
     date_label.pack()
-    update_button = ttk.Button(task_frame, command=lambda: update_frame(name_label, date_label))
+    update_button = ttk.Button(task_frame, command=lambda: update_frame(name_label, date_label, temp, all_task[0], all_task[1]))
     update_button["text"] = "Update"
     update_button.pack()
-    delete_button = ttk.Button(task_frame, command=lambda: delete_frame(task_frame))
+    delete_button = ttk.Button(task_frame, command=lambda: delete_frame(task_frame, temp, all_task[0], all_task[1]))
     delete_button["text"] = "Delete"
     delete_button.pack()
     all_task = ["No_Name", "No_Date", "To Do"]
@@ -133,9 +183,19 @@ def add_task_button(todo_frame, doing_frame, done_frame):
 
 
     root.mainloop()
-def delete_frame(frame):
+def delete_frame(frame, clmn, name, date):
+    if clmn == "To Do":
+        share_todo.remove([name, date])
+    if clmn == "Doing":
+        share_doing.remove([name, date])
+    if clmn == "Done":
+        share_done.remove([name, date])
     frame.destroy()
+<<<<<<< Updated upstream
 def update_frame(name_label, date_label):
+=======
+def update_frame(name_label, date_label, clmn, name, date):
+>>>>>>> Stashed changes
     update = Tk()
     label1 = ttk.Label(update)
     label1["text"] = "TaskName: "
@@ -151,12 +211,12 @@ def update_frame(name_label, date_label):
 
     text_field1 = ttk.Entry(update)
     text_field1.grid(row=0, column=1)
-    submit_1 = ttk.Button(update, command=lambda: update_entry(text_field1, name_label))
+    submit_1 = ttk.Button(update, command=lambda: update_entry(text_field1, name_label, 1, clmn, name, date))
     submit_1.grid(row=0, column=2)
 
     text_field2 = ttk.Entry(update)
     text_field2.grid(row=1, column=1)
-    submit_2 = ttk.Button(update, command=lambda: update_entry(text_field2, date_label))
+    submit_2 = ttk.Button(update, command=lambda: update_entry(text_field2, date_label, 2, clmn, name, date))
     submit_2.grid(row=1, column=2)
 
     combo_box1 = ttk.Combobox(update)
@@ -166,8 +226,44 @@ def update_frame(name_label, date_label):
 
     combo_box1.grid(row=2, column=1)
     update.mainloop()
-def update_entry(event, label):
+def update_entry(event, label, num, clmn, name, date):
     label["text"] = event.get()
+    if num == 1:
+        if clmn == "To Do":
+            share_todo.remove([name, date])
+            share_todo.append([label, date])
+        if clmn == "Doing":
+            share_doing.remove([name, date])
+            share_doing.append([label, date])
+        if clmn == "Done":
+            share_done.remove([name, date])
+            share_done.append([label, date])
+    if num == 2:
+        if clmn == "To Do":
+            share_todo.remove([name, date])
+            share_todo.append([name, label])
+        if clmn == "Doing":
+            share_doing.remove([name, date])
+            share_doing.append([name, label])
+        if clmn == "Done":
+            share_done.remove([name, date])
+            share_done.append([name, label])
+
+def share_tasks():
+    print(share_todo)
+    with open("output.csv", 'w') as f:
+        wrtr = csv.writer(f)
+        head = ["Column", "Name", "Date"]
+        wrtr.writerow(head)
+        for i in share_todo:
+            temp = ["To Do", i[0], i[1]]
+            wrtr.writerow(temp)
+        for i in share_doing:
+            temp = ["Doing", i[0], i[1]]
+            wrtr.writerow(temp)
+        for i in share_todo:
+            temp = ["Done", i[0], i[1]]
+            wrtr.writerow(temp)
 
 def main():
     f = open("db1.txt", "a")
@@ -207,6 +303,15 @@ def main():
     add_button["text"] = "Add Task"
     add_button.grid(row=0, column=2)
 
+<<<<<<< Updated upstream
+=======
+    share_button = ttk.Button(app, command=lambda: share_tasks())
+    share_button["text"] = "Share"
+    share_button.grid(row=0, column=0)
+
+# format each task frame
+
+>>>>>>> Stashed changes
     todo_frame = Frame(app, highlightcolor="pink", highlightbackground="pink", highlightthickness=5)
     todo_frame.grid(row=2, column=0)
 
